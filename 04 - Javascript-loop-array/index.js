9ae0a196cf5502b65932f0c7bdfcbdceb020d479
unknown <mazandrerukmana22@gmail.com>
2023-09-01 13:19:09 +0700

    Push Tugas 4

---
+++ 04 - Javascript-loop-array/index.js
@@ -0,0 +1,74 @@
+/*Soal 1
+
+for(var angka = 1; angka < 10; angka++) {
+    console.log(angka)
+  }
+
+*/
+
+
+/*Soal 2 Genap
+
+for(let angka = 0; angka < 10; angka+=2) {
+    console.log(angka)
+    }
+
+*/
+
+
+/* Soal 3 Ganjil 
+for(var angka = 1; angka < 10; angka+=2) {
+    console.log(angka)
+    }
+    
+*/
+
+/*Soal 4 
+
+let array1 = [1,2,3,4,5,6]
+console.log( array1[5])
+
+*/
+
+/* Soal 5 
+
+let array2 = [5,2,4,1,3,5]
+array2.sort()
+console.log(array2)
+
+*/
+
+/* Soal 6 
+let array3 = ["selamat", "anda", "melakukan", "perulangan", "array", "dengan", "for"]
+	  for(let i = 0; i < array3.length; i++) {
+	       console.log(array3[i]);
+          }
+*/
+
+/* Soal 7 
+
+let array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for(let i = 1; i < array4.length; i+=2) {
+    console.log(array4[i]);
+   }
+
+*/
+
+/*Soal 8
+
+let kalimat= ["saya", "sangat", "senang", "belajar", "javascript"]
+var combine = kalimat.join(" ")
+console.log(combine)
+
+*/
+
+/* Soal 9 
+let sayuran = []
+let sayur= ['Kangkung', 'Bayam',
+'Buncis' ,'Kubis',
+'Timun' , 'Seledri' ,
+'Tauge']
+sayuran.push(sayur)
+console.log(sayuran)
+
+*/
