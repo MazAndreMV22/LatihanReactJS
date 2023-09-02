13d956f6fd1d3ccaf91597ec93fe468441ab179b
MazAndreMV22 <133617570+MazAndreMV22@users.noreply.github.com>
2023-09-02 12:56:34 +0700

    Add files via upload
    Push Tugas 5

---
+++ 05 - Javascript-Function-Object/tugas5.js
@@ -0,0 +1,106 @@
+/*Soal 1
+
+function cetakFunction() {
+ return "HALO! Nama Saya Andre Rukmana" 
+  }
+console.log(cetakFunction())
+
+*/
+
+/*Soal 2
+function myFunction() {
+return angka1 + angka2
+  
+}
+let angka1 = 20
+let angka2 = 7
+var output = myFunction(angka1, angka2)
+console.log(output)
+
+*/
+
+/*Soal 3
+const Hello = () => {
+
+  return "Hello"
+  
+  }
+
+Hello()
+
+*/
+
+/* Soal 5 
+let  obj = {
+
+  nama : "john",
+  
+  umur : 22,
+  
+  bahasa : "indonesia"
+  
+  }
+  console.log(obj.bahasa)
+
+  */
+
+  /* Soal 6 
+
+var buah = [{  nama: "Nanas",warna: "Kuning",adaBijinya: false,harga: "9000" },
+            {  nama: "Jeruk",warna: "Oranye",adaBijinya: true,harga: "8000"},
+            {  nama: "Semangka",warna: "Hijau & Merah",adaBijinya: true,harga: "10000"},
+            {  nama: "Pisang",warna: "Kuning",adaBijinya: false,harga: "5000"}]
+
+var buahfilter = buah.filter(function(item){
+return item.adaBijinya == true ; })              
+console.log(buahfilter)
+*/
+
+/* Soal 7 
+let phone = {
+  name: "Galaxy Fold 5",
+  brand: "Samsung",
+  year: 2023
+} ;
+const {name, brand, year} = phone
+console.log(name, brand, year)
+*/
+
+/* Soal 8
+
+let dataBukuTambahan= {
+  penulis: "john doe",
+  tahunTerbit: 2020 
+}
+
+let buku = {
+  nama: "pemograman dasar",
+  jumlahHalaman: 172
+}
+
+let objOutput = {...dataBukuTambahan, ...buku
+}
+
+// kode diatas ini jangan di rubah atau di hapus sama sekali
+
+console.log(objOutput) 
+
+*/
+
+/* Soal 9
+
+let mobil = [{
+  merk : "bmw",
+  color: "red",
+  year : 2002
+  
+  }]
+  const functionObject = (mobil) => {
+  return mobil.merk == "bmw" ;
+  
+  }
+  functionObject(); 
+
+  */
+
+  
